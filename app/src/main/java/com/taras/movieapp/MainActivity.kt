c232c4f4cfe9ab74839ea3941de94e0ca0c9e21b
Taras Koloshmatin <taras@pier2marketing.com>
2019-03-10 19:40:00 +0200

    search movie

--- app/src/main/java/com/taras/movieapp/MainActivity.kt
+++ app/src/main/java/com/taras/movieapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.taras.movieapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.taras.movieapp.content.ContentPagerFragment
 
@@ -13,5 +15,18 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.content, ContentPagerFragment.newInstance())
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/taras/movieapp/content/SearchMovieActivity.kt
@@ -0,0 +1,13 @@
+package com.taras.movieapp.content
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.taras.movieapp.R
+
+class SearchMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_movie)
+    }
+}
